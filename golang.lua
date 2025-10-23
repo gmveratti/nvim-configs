6573e904f1da7a55725aca99fcb12ee3f8ce600b
Gabriel Veratti <134892354+gmveratti@users.noreply.github.com>
2025-10-23 15:02:18 -0300

    Refactor go.nvim plugin configuration
    Updated configuration for ray-x/go.nvim plugin with additional dependencies and options.

--- golang.lua
+++ golang.lua
@@ -1,21 +1,26 @@
--- ~/.config/nvim/lua/plugins/go.lua
-
-return {
-	"ray-x/go.nvim",
-	dependencies = {
-		"neovim/nvim-lspconfig",
-		"nvim-treesitter/nvim-treesitter",
-		"mfussenegger/nvim-dap",
-	},
-	config = function()
-		require("go").setup({
-			-- Descomente para usar goimports em vez de gofmt ao salvar
-			-- gofmt = "goimports",
-		})
-	end,
-	-- Carrega o plugin para arquivos Go ou ao entrar na linha de comando
-	event = { "CmdlineEnter" },
-	ft = { "go", "gomod" },
-	-- Comando para instalar/atualizar as ferramentas do Go (gopls, delve, etc.)
-	build = ':lua require("go.install").update_all_sync()',
+{
+  "ray-x/go.nvim",
+  dependencies = {  -- optional packages
+    "ray-x/guihua.lua",
+    "neovim/nvim-lspconfig",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  opts = {
+    -- lsp_keymaps = false,
+    -- other options
+  },
+  config = function(lp, opts)
+    require("go").setup(opts)
+    local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*.go",
+      callback = function()
+      require('go.format').goimports()
+      end,
+      group = format_sync_grp,
+    })
+  end,
+  event = {"CmdlineEnter"},
+  ft = {"go", 'gomod'},
+  build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
 }
